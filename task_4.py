c3078cd21b71b9de7b79d887b6e476cf7974b906
mwatakazembe <kill.va.udaki@gmail.com>
2025-09-30 06:34:07 +0300

    decyclification2

--- task_4.py
+++ task_4.py
@@ -1,5 +1,24 @@
 sum = int(input("amount of money: "))
-for i in [100, 50, 20, 10, 5, 2, 1]:
-    count = sum // i
-    print(f"{i}: {count}")
-    sum %= i
+
+banknote100 = sum // 100
+remain = sum % 100
+
+banknote50 = remain // 50
+remain = remain % 50
+
+banknote10 = remain // 10
+remain = remain % 10
+
+banknote5 = remain // 5
+remain = remain % 5
+
+banknote2 = remain // 2
+banknote1 = remain % 2
+
+print("you'll need banknotes of:")
+print(f"100: {banknote100}")
+print(f"50: {banknote50}")
+print(f"10: {banknote10}")
+print(f"5: {banknote5}")
+print(f"2: {banknote2}")
+print(f"1: {banknote1}")
