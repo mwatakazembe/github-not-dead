395a91ee973a9f2822676b86de73bfb97d64afbb
mwatakazembe <kill.va.udaki@gmail.com>
2025-11-24 05:39:14 +0300

    t3

---
+++ lab52/task3.py
@@ -0,0 +1,27 @@
+def is_palindrome(s):
+    s = str(s)
+    return s == s[::-1]
+
+def test_palindrome_number_positive():
+    assert is_palindrome(121) == True
+
+def test_palindrome_number_negative():
+    assert is_palindrome(-121) == False
+
+def test_palindrome_string_positive():
+    assert is_palindrome("aba") == True
+
+def test_palindrome_string_negative():
+    assert is_palindrome("abc") == False
+
+def test_palindrome_empty_string():
+    assert is_palindrome("") == True
+
+def test_palindrome_single_char():
+    assert is_palindrome("a") == True
+
+def test_palindrome_mixed_case():
+    assert is_palindrome("Anna") == False
+
+def test_palindrome_with_spaces():
+    assert is_palindrome("a b a") == False
