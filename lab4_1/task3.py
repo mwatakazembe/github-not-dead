5e82ff26b5f2806d7e000e4c77896b148981cf4a
mwatakazembe <kill.va.udaki@gmail.com>
2026-02-18 13:58:06 +0300

    turtle

--- lab4_1/task3.py
+++ lab4_1/task3.py
@@ -12,16 +12,19 @@ head = patches.Ellipse(xy = (0.5,0.57), width=0.17, height=0.6, facecolor='#b3f5
 eye1 = patches.Circle((0.4565, 0.83), radius=0.009, facecolor='black')
 eye2 = patches.Circle((0.5424, 0.83), radius=0.009, facecolor='black')
 tail = patches.Polygon([(0.5, 0.16), (0.49, 0.2), (0.51, 0.2)], closed=True, facecolor='#b3f542', edgecolor='black')
+tongue = patches.Polygon([(0.5, 0.86), (0.495, 0.89), (0.505, 0.89)], closed = True, facecolor='fuchsia', edgecolor = 'black')
 
 ax.add_patch(leg1)
 ax.add_patch(leg2)
 ax.add_patch(leg3)
 ax.add_patch(leg4)
+ax.add_patch(tongue)
 ax.add_patch(head)
 ax.add_patch(body)
 ax.add_patch(tail)
 ax.add_patch(eye1)
 ax.add_patch(eye2)
+
 
 ax.set_xlim(0, 1)
 ax.set_ylim(0, 1)
